72bfe813f383c78fbd4d464af82dd0afb3f88099
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-09-04 10:42:23 +0200

    ajout fichiers

--- tuto/views.py
+++ tuto/views.py
@@ -1,5 +1,12 @@
 from .app import app
 from flask import render_template
+import yaml, os.path
+
+data = yaml.safe_load(
+                open(
+                    os.path.join(
+                        os.path. dirname ( __file__ ),
+                        "data.yml")))
 
 @app.route("/")
 def home ():
