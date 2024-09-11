69dbeec6d8f1a5e098f9dfff59e7d93f0d4ad8f4
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-09-11 09:38:40 +0200

    modifications code

--- tuto/views.py
+++ tuto/views.py
@@ -1,16 +1,10 @@
 from .app import app
 from flask import render_template
-import yaml, os.path
-
-data = yaml.safe_load(
-                open(
-                    os.path.join(
-                        os.path. dirname ( __file__ ),
-                        "data.yml")))
+from .models import get_sample
 
 @app.route("/")
 def home():
     return render_template (
         "home.html",
-        title="Hello World!",
-        names =["Pierre", "Paul", " Corinne "])
+        title="My Books",
+        books =get_sample())
