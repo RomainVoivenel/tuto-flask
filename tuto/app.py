7a7a89a6caa266beba005b44c41b40281ea5f381
Romain Voivenel <romain.voivenel@etu.univ-orleans.fr>
2024-09-18 09:56:24 +0200

    modification pour utiliser SQLAlchemy et ajout de classes

--- tuto/app.py
+++ tuto/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+import os.path
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 bootstrap = Bootstrap5(app)
+
+def mkpath(p):
+    return os.path.normpath(os.path.join(os.path.dirname(__file__), p))
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['QLALCHEMY_DATABASE_UR'] = (
+    'sqlite:///'+mkpath('../myapp.db')
+)
+db = SQLAlchemy(app)
